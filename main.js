af032e99f00caac9a2052d575cb01763c0915a6e
Newspaper Reports <banglacomedystore@gmail.com>
2025-03-05 23:23:59 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+function openSearch() {
+    document.getElementById('searchWorld').style.width = "100%";
+};
+
+function closeWorld() {
+    document.getElementById('searchWorld').style.width = "0%";
+};
+// End Global Search Icon //
+
+
+// Responsive Mobile Script Area //
+function openMbCgList() {
+    document.getElementById('openMbSlideMenu').style.width = "320px"
+}
+
+function closeMbCgList() {
+    document.getElementById('openMbSlideMenu').style.width = "0px"
+};
+
+// Responsive Search Area //
+function openMbSearchWorld() {
+    document.getElementById('mb_searchWorld').style.width = "100%"
+}
+
+function closeMbSearchWorld() {
+    document.getElementById('mb_searchWorld').style.width = "0%"
+};
+
+
+// End Responsive Mobile Script Area //
